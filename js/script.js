ec9bd4d98a5f9bfd10a86ca75b95db9b6cf0509c
AndreazzaRiccardo <riccardo.andreazza90@gmail.com>
2023-11-06 17:46:14 +0100

    add: objects array

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const ourTeam = [
+    {
+        name: "Wayne Barnett",
+        profession: "Founder & CEO",
+        photo: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        profession: "Chief Editor",
+        photo: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        profession: "Office Manager",
+        photo: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        profession: "Social Media Manager",
+        photo: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        profession: "Developer",
+        photo: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        profession: "Graphic Designer",
+        photo: " barbara-ramos-graphic-designer.jpg"
+    }
+]
